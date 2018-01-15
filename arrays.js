76d1b20b468347b29fc33941309e1a5ef7e64eeb
Jabulani Msimango <msimango.jabulani@moov.life>
2018-01-15 12:41:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skitles']
+
+function addElementToBeginningOfArray(arrayToEdit[], element) {
+  arrayToEdit.unshift(element);
+  return arrayToEdit;
+}
+
+function destructivelyAddElementToBeginningOfArray(arrayToEdit[], element) {
+  var newArray = [element, ...arrayToEdit]
+}
